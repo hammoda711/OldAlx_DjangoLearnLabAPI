8204470c4ebf31f37266a2a70f8d77304c65bc69
Mohamed Taher <mo.taher717@gmail.com>
2024-09-16 22:09:48 +0300

    solve small bug

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,10 @@
 
 
 from django.urls  import path
-from .views import BookListAPIView
+from .views import BookListAPIView as BookList
 
 urlpatterns = [
     # Maps to the BookList view 
     # http://127.0.0.1:8000/api/books/
-    path('books/', BookListAPIView.as_view(), name='book-list'),  
+    path('books/', BookList.as_view(), name='book-list'),  
 ]
